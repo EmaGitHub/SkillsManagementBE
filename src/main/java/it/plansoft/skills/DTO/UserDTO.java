82ac4c7f037267ac707ecd5345c7c924c886fe6e
EmaGitHub <emanuele.calvisi@gmail.com>
2021-07-02 10:36:47 +0200

    lombok

--- src/main/java/it/plansoft/skills/DTO/UserDTO.java
+++ src/main/java/it/plansoft/skills/DTO/UserDTO.java
@@ -1,95 +1,41 @@
 package it.plansoft.skills.DTO;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.sql.Date;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 import it.plansoft.skills.Model.BaseModel;
 
 @Entity
-@Table(name = "USER")
+@Table(name = "user")
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO extends BaseModel<Long>{
 
 	@Id
+	@Getter @Setter
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", nullable = false, unique = true)
 	private Long id;
+	@Getter @Setter
 	@Column(name = "username", nullable = false)
 	private String username;
+	@Getter @Setter
 	@Column(name = "password", nullable = false)
 	private String password;
+	@Getter @Setter
 	@Column(name = "first_name", nullable = true)
 	private String firstName;
+	@Getter @Setter
 	@Column(name = "last_name", nullable = true)
 	private String lastName;
+	@Getter @Setter
 	@Column(name = "dt_insert", nullable = true)
 	private java.util.Date  dtInsert;
+	@Getter @Setter
 	@Column(name = "is_system_admin", nullable = true)
 	private Boolean isSystemAdmin;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String name) {
-		this.firstName = name;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public java.util.Date getDtInsert() {
-		return dtInsert;
-	}
-
-	public void setDtInsert(java.util.Date date) {
-		this.dtInsert = date;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public Boolean getIsSystemAdmin() {
-		return isSystemAdmin;
-	}
-
-	public void setIsSystemAdmin(Boolean isSystemAdmin) {
-		this.isSystemAdmin = isSystemAdmin;
-	}
-
 }
